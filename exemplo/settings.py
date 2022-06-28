abeeac44654ceff74e5ac3bf08b6a03dfba84062
Manuel Viana <manuel.viana@gmail.com>
2022-06-28 19:13:28 +0100

    Django Hello

--- exemplo/settings.py
+++ exemplo/settings.py
@@ -78,6 +78,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'sn_mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SOCIAL_NETWORK',
+        'USER': 'root',
+        'PASSWORD': '',
+         'HOST': '127.0.0.1',
+         'PORT': '3306',
+         'OPTIONS': {
+             'init_command': "SET sql_model='STRICT_TRANS_TABLES'"
+         }
     }
 }
 
--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print("test")
+from getpass import getpass
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        host='192.168.64.2', # '127.0.0.1',
+        user='teste', # 'root',
+        password='teste', # '',
+        db='SOCIAL_NETWORK',    
+    ) as connection:
+        query='SELECT * FROM USER'
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            for db in cursor:
+                print(db)
+except Error as e:
+    print(e)
+    
+# print("test")
